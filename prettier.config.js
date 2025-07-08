c2c5f31c7c948264766ed9275187032831ae9892
yasmim_fariaa <yasmimstefanefaria@gmail.com>
2025-07-08 16:24:02 -0300

    subindo trabalho

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Config} */
+export default {
+    semi: true,             // Usa ponto e virgula no final das linhas
+    singleQuote: true,      // Usa aspas simples em vez de duplas
+    useTabs: false,         // Usa espaços em vez de tabs
+    bracketSpacing: true,   // Espaço entre chaves em objetos { foo: bar }
+    arrowParens: 'always',  // Sempre usa parenteses em funções arrow
+    endOfLine: 'auto',      // Mantém o estilo de fim de linha do sistema
+};
